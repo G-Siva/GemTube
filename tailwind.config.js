6d7d12e7cde89cf692b56cd67b1875906d847fd9
G-Siva <111060300+G-Siva@users.noreply.github.com>
2024-08-03 14:46:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': {'max': '639px'},
+        'md': {'max': '767px'},
+        'lg': {'max': '1023px'},
+        'xl': {'max': '1279px'},
+      },
+    },
+  },
+  plugins: [],
+};
